9d1369dd7a46bf89e6cc45b1f217200a645fcb01
Namandeepsd <namands.04@gmail.com>
2024-10-17 19:35:35 +0530

    C++ code to print a diamond-like star pattern using nested loops

--- OpenSourcery.cpp
+++ OpenSourcery.cpp
@@ -28,3 +28,34 @@ P.S.: your filename in the pull request should only be made in the form of yourN
 */
 
 // Write Code Here
+#include <iostream>
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 10; j >= 1; j--) {
+            if (j > i && j < (11 - i)) {
+                printf(" ");
+            } else {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+
+    for (int i = 6; i < 10; i++) {
+        for (int j = 10; j > 0; j--) {
+            if (j > i || j < (11 - i)) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
