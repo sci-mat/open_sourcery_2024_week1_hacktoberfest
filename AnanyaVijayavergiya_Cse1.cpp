86f4fb4b69ec4657fa1b75f736261dcba573f268
[Ananyav2809] <[ananya.vijayavergiya.24cse@bmu.edu.in]>
2024-10-20 11:16:37 +0530

    File adde by Ananya Vijayavergiya

--- AnanyaVijayavergiya_Cse1.cpp
+++ AnanyaVijayavergiya_Cse1.cpp
@@ -1,48 +1,24 @@
 /*
-
-Week 1 Task: Write a C++ Code to print the following butterfly pattern 
-
-*       *
-**     **
-***   ***
-**** ****
-*********
-**** ****
-***   ***
-**     **
-*       *
-
-Task Deadline: 21st October 2024
-
-Cheatsheet: 
-
-1. git add .
-
-2. git commit -m "your_message"
-
-3. git push origin main
-
-P.S.: your filename in the pull request should only be made in the form of yourName_batch
-
+** updated by Ananya Vijayavergiya btech cse1
 */
-
-// Write Code Here
-
 #include <iostream>
 using namespace std;
 
 int main() {
-    int n = 5;
+    int n = 8;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= i; j++) {
             cout << "*";
         }
+
         for (int j = 1; j <= 2 * (n - i); j++) {
             cout << " ";
         }
+
         for (int j = 1; j <= i; j++) {
             cout << "*";
         }
+
         cout << endl;
     }
 
@@ -50,12 +26,15 @@ int main() {
         for (int j = 1; j <= i; j++) {
             cout << "*";
         }
+
         for (int j = 1; j <= 2 * (n - i); j++) {
             cout << " ";
         }
+
         for (int j = 1; j <= i; j++) {
             cout << "*";
         }
+
         cout << endl;
     }
 
