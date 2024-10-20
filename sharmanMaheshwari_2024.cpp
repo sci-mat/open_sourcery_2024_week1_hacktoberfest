a23fa061082360aaf5216c8aadfcceb754650065
[Ananyav2809] <[ananya.vijayavergiya.24cse@bmu.edu.in]>
2024-10-20 10:55:23 +0530

    tested by updating comments

--- sharmanMaheshwari_2024.cpp
+++ sharmanMaheshwari_2024.cpp
@@ -11,7 +11,7 @@ Week 1 Task: Write a C++ Code to print the following butterfly pattern
 ***   ***
 **     **
 *       *
-
+** updated by Ananya Vijayavergiya
 Task Deadline: 21st October 2024
 
 Cheatsheet: 
@@ -21,7 +21,6 @@ Cheatsheet:
 2. git commit -m "your_message"
 
 3. git push origin main
-
 
 P.S.: your filename in the pull request should only be made in the form of yourName_batch
 
