751f1a32c1832f0b1e5efba4f202c0d57cfe72bd
Aaryamantwix <twixgg77@gmail.com>
2024-10-20 19:59:29 +0530

    Create Aaryamanpratapsingh_Cse1.cpp

---
+++ Aaryamanpratapsingh_Cse1.cpp
@@ -0,0 +1,72 @@
+/*
+
+Week 1 Task: Write a C++ Code to print the following butterfly pattern 
+
+*       *
+**     **
+***   ***
+**** ****
+*********
+**** ****
+***   ***
+**     **
+*       *
+
+Task Deadline: 21st October 2024
+
+Cheatsheet: 
+
+1. git add .
+
+2. git commit -m "your_message"
+
+3. git push origin main
+
+
+P.S.: your filename in the pull request should only be made in the form of yourName_batch
+
+*/
+
+// Write Code Here
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5; 
+    
+    for (int i = 1; i <= n; i++) {
+        
+        for (int j = 1; j <= i; j++) {
+            cout << "*"; 
+        }
+        
+        for (int j = 1; j <= 2 * (n - i); j++) {
+            cout << " "; 
+        }
+       
+        for (int j = 1; j <= i; j++) {
+            cout << "*"; 
+        }
+        cout << endl; 
+    }
+
+    
+    for (int i = n; i >= 1; i--) {
+        
+        for (int j = 1; j <= i; j++) {
+            cout << "*"; 
+        }
+        
+        for (int j = 1; j <= 2 * (n - i); j++) {
+            cout << " "; 
+        }
+        
+        for (int j = 1; j <= i; j++) {
+            cout << "*"; 
+        }
+        cout << endl; 
+    }
+
+    return 0; 
+}
